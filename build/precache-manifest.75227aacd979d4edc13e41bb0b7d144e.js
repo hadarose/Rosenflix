438c2dbccb9b4db4cbbf838ca7adf922eec12cb8
hadarose <hadarose@gmail.com>
2020-12-04 20:21:49 +0200

    Adding dist folder & Procfile to the repository

---
+++ build/precache-manifest.75227aacd979d4edc13e41bb0b7d144e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42169d426171f601cb0052f79253b2fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "250705cb80451ad776d8",
+    "url": "/static/css/main.4347bb24.chunk.css"
+  },
+  {
+    "revision": "09f8a9bee0e3ba907a33",
+    "url": "/static/js/2.ae866ad9.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.ae866ad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "250705cb80451ad776d8",
+    "url": "/static/js/main.c985590c.chunk.js"
+  },
+  {
+    "revision": "44e811e0071c3d124d9e",
+    "url": "/static/js/runtime-main.3557df50.js"
+  }
+]);
